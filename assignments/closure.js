a51780cc11c14ee97d46877547f6a7684f2bf055
Lindsey Cason <lindseyacason@gmail.com>
2019-07-23 18:22:55 -0500

    final for JSII

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,5 +1,12 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+let location = "I'm taking over the world!"
+function findMe(where){
+console.log(where);
+}
+
+  findMe(location);
+
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
